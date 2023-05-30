9bc792360d9cb65e06be0880f52297daf5f06a99
Cristopher <cristopher15montoya@hotmail.com>
2023-05-30 17:27:09 -0600

    Primer commit del proyecto

---
+++ build/classes/posprogra2/POSProgra2.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package posprogra2;
+import GUI.VentanaIni;
+/**
+ *
+ * @author unicomer
+ */
+public class POSProgra2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+        VentanaIni Ver = new VentanaIni();
+        Ver.setVisible(true);
+
+    }
+
+}
